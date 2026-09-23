d3ff3502b5f33a717f09ab3c86be1b7b757a5688
dagamoner <dagamoner@gmail.com>
2026-09-23 12:41:31 -0300

    content: Update third footer link to Mendoza por Mi with correct URL

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -19,8 +19,8 @@ export default function Footer() {
           <a href="https://www.mendoza.gov.ar/" target="_blank" rel="noopener noreferrer" className="hover:text-white transition-colors underline decoration-white/30 underline-offset-4">
             Gobierno de Mendoza
           </a>
-          <a href="https://www.mendoza.gov.ar/" target="_blank" rel="noopener noreferrer" className="hover:text-white transition-colors underline decoration-white/30 underline-offset-4">
-            mendoza.gov.ar
+          <a href="https://id.mxm.mendoza.gov.ar/realms/mendoza_x_mi/protocol/openid-connect/auth?client_id=mxm_portal&scope=openid%20email%20profile&response_type=code&redirect_uri=https%3A%2F%2Fmxm.mendoza.gov.ar%2Fapi%2Fauth%2Fcallback%2Fkeycloak&state=umBiYrNUWYWLe_efphQxu2XBPKmCab9EntotJwBre2Y&code_challenge=heunD2jMJCNXzI_n14PMaYRpNb72qD2p2VGSW19YvQ8&code_challenge_method=S256" target="_blank" rel="noopener noreferrer" className="hover:text-white transition-colors underline decoration-white/30 underline-offset-4">
+            Mendoza por Mi
           </a>
         </div>
         
